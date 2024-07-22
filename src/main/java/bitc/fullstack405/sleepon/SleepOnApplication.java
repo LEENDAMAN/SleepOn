b6f50350298a5b8b5909db70f9ec5c750e82e8ab
LEENDAMAN <bath3479@gmail.com>
2024-07-22 14:34:48 +0900

    SleepOn 생성

---
+++ src/main/java/bitc/fullstack405/sleepon/SleepOnApplication.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.sleepon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepOnApplication.class, args);
+    }
+
+}
